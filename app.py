a358ad9b09b84085378781eb7b6d6d86a1b808e1
ryang42 <ryang42@kprschools.ca>
2025-04-07 09:32:08 -0400

    started connection

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
-#test again
-#test 2
+import sqlite3
+
+connection = sqlite3.connect("data.db")
+
+with connection:
+    connection.execute("CREATE TABLE beans(id INTEGER PRIMARY KEY, name TEXT, method TEXT, rating INTEGER);")
