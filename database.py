e99da614d2567bee3528f67dbcffb7fb0a1a12ad
ryang42 <ryang42@kprschools.ca>
2025-04-08 09:13:37 -0400

    22.52 in vid

--- database.py
+++ database.py
@@ -7,6 +7,7 @@ INSERT_BEAN = "INSERT INTO beans (name, method, rating) VALUES (?, ?, ?);"
 GET_ALL_BEANS = "SELECT * FROM beans;"
 GET_BEANS_BY_NAME = "SELECT * FROM beans WHERE name = ?;"
 
+
 def connect():
     return sqlite3.connect("data.db")
 
@@ -17,3 +18,11 @@ def create_tables(connection):
 def add_bean(connection, name, method, rating):
     with connection:
         connection.execute(INSERT_BEAN, (name, method, rating))
+
+def get_all_beans(connection):
+    with connection:
+        return connection.execute(GET_ALL_BEANS).fetchall()
+
+def get_beans_by_name(connection, name):
+    with connection:
+        connection.execute(GET_BEANS_BY_NAME, (name,)).fetchall()
